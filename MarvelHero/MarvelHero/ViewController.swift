d9771c8a8f2a13a1e88f33374f85dee6b4151afd
Annitas <39757257+Annitas@users.noreply.github.com>
2024-03-05 10:01:52 +0600

    Initial commit

---
+++ MarvelHero/MarvelHero/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarvelHero
+//
+//  Created by Anita Stashevskaya on 05.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
